e13bb1e3b72c3876e979e57b9b6fcabbff4d5624
mchrapkowicz <mchrapkowicz@consdata.pl>
2019-09-05 20:15:53 +0200

    blueprint works now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import {Button, Card, Elevation} from '@blueprintjs/core';
 import React from 'react';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import './App.css';
 
 const App: React.FC = () => {
