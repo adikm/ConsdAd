2d6c0a8f9229bb4e2331ae06af6d190f4d7a48c0
adrian <adrmarszalek@gmail.com>
2019-09-05 22:43:04 +0200

    endpointy do mongo

--- backend/src/main/kotlin/net/flamingos/consdad/MainVerticle.kt
+++ backend/src/main/kotlin/net/flamingos/consdad/MainVerticle.kt
@@ -1,17 +1,33 @@
 package net.flamingos.consdad
 
 import io.vertx.core.*
+import io.vertx.core.http.HttpMethod
+import io.vertx.core.json.*
+import io.vertx.ext.web.*
+import io.vertx.ext.web.handler.BodyHandler
+import java.util.stream.Collectors
 
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
+    val mongoConnector = MongoConnector(vertx);
+    val router = Router.router(vertx)
+
+    router.get("/").handler { routingContext ->
+      val adId = routingContext.queryParams().get("id")
+      when {
+          adId != null -> mongoConnector.get(adId, getOneCallback(routingContext))
+          else -> mongoConnector.getAll(getAllCallback(routingContext))
+      }
+    }
+    router.post("/").handler(BodyHandler.create()).handler { routingContext ->
+      val advertisement = jsonObjectToAdvertisement(routingContext.bodyAsJson)
+      mongoConnector.save(advertisement, saveCallback(routingContext));
+    }
+
     vertx
       .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
+      .requestHandler(router)
       .listen(8888) { http ->
         if (http.succeeded()) {
           startPromise.complete()
@@ -20,5 +36,38 @@ class MainVerticle : AbstractVerticle() {
           startPromise.fail(http.cause());
         }
       }
+  }
+
+  fun getAllCallback(routingContext: RoutingContext): (event: AsyncResult<MutableList<JsonObject>>) -> Unit =
+    {
+      val ads = it.result().stream().map { jsonObjectToAdvertisement(it) }.collect(Collectors.toList());
+      routingContext.response()
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(ads))
+    }
+
+  fun getOneCallback(routingContext: RoutingContext): (event: AsyncResult<JsonObject>) -> Unit =
+    {
+      routingContext.response()
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(it.result()))
+    }
+
+
+  fun saveCallback(routingContext: RoutingContext): (event: AsyncResult<String>) -> Unit =
+    {
+      routingContext.response()
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(it.result())
+    }
+
+
+  private fun jsonObjectToAdvertisement(jsonObject: JsonObject): Advertisement {
+    val tags = mutableListOf<String>()
+    jsonObject.getJsonArray("tags")?.forEach { tag ->
+      tags.add(tag.toString())
+    }
+    return Advertisement(jsonObject.getJsonObject("_id").getString("\$oid"), jsonObject.getString("title"), "",
+      jsonObject.getString("content"), tags)
   }
 }
--- backend/src/main/kotlin/net/flamingos/consdad/MongoConnector.kt
+++ backend/src/main/kotlin/net/flamingos/consdad/MongoConnector.kt
@@ -1,11 +1,13 @@
 package net.flamingos.consdad;
 
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.json.JsonObject
+import io.vertx.core.*
+import io.vertx.core.json.*
 import io.vertx.ext.mongo.MongoClient
+import io.vertx.ext.web.RoutingContext
+import java.util.stream.Collectors
 
 
-object MongoConnector : AbstractVerticle() {
+class MongoConnector(vertx: Vertx) {
 
   val mongoconfig = JsonObject()
     .put("connection_string", "mongodb+srv://corkdata:Haslo12345@cluster0-w9o8m.mongodb.net/test?retryWrites=true&w=majority")
@@ -13,4 +15,22 @@ object MongoConnector : AbstractVerticle() {
 
   val mongoClient = MongoClient.createShared(vertx, mongoconfig)
 
+  private val COLLECTION = "ads"
+
+  fun getAll(callback: (event: AsyncResult<MutableList<JsonObject>>) -> Unit) {
+    mongoClient.find(COLLECTION, JsonObject(), callback)
+  }
+
+  fun get(id: String, callback: (AsyncResult<JsonObject> ) -> Unit) {
+    mongoClient.findOne(COLLECTION, JsonObject().put("_id", id), null, callback)
+  }
+
+  fun save(advertisement: Advertisement, callback: (event: AsyncResult<String>) -> Unit) {
+    mongoClient.insert(COLLECTION, advertisementToJsonObject(advertisement), callback)
+  }
+
+  private fun advertisementToJsonObject(advertisement: Advertisement) = JsonObject()
+    .put("content", advertisement.content)
+    .put("title", advertisement.title)
+    .put("tags", advertisement.tags)
 }
